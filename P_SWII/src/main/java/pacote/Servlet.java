222ce268cbde1d4e9d0919ab08ab19a321cfa540
João Cardoso <jg235711p@gmail.com>
2025-03-06 17:36:12 -0300

    Update Servlet.java

--- P_SWII/src/main/java/pacote/Servlet.java
+++ P_SWII/src/main/java/pacote/Servlet.java
@@ -1,11 +1,11 @@
 package pacote;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
+import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import java.io.IOException;
 
@@ -37,11 +37,12 @@ public class Servlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int ano = Integer.parseInt(
 				request.getParameter("ano"));
-
+		String nome = request.getParameter("nome");
 		int idade = 2025 - ano;
 		
 		HttpSession session = request.getSession();
 		session.setAttribute("idade", idade);
+		session.setAttribute("nome", nome);
 		response.sendRedirect("./resp.jsp");
 	}
 }
